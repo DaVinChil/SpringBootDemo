a11617fcf612e82b3db321dfecfc3b256f119214
DaVinCi <fox.winner9@gmail.com>
2023-06-08 18:18:34 +0300

    Complete

--- src/main/java/com/example/demo/configuration/JavaConfig.java
+++ src/main/java/com/example/demo/configuration/JavaConfig.java
@@ -1,8 +1,25 @@
 package com.example.demo.configuration;
 
+import com.example.demo.profiles.DevProfile;
+import com.example.demo.profiles.ProductionProfile;
+import com.example.demo.profiles.SystemProfile;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JavaConfig {
 
+    @Bean
+    @ConditionalOnProperty("netology.profile.dev")
+    public SystemProfile devProfile(){
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(SystemProfile.class)
+    public SystemProfile prodProfile(){
+        return new ProductionProfile();
+    }
 }
--- src/main/java/com/example/demo/profiles/SystemProfile.java
+++ src/main/java/com/example/demo/profiles/SystemProfile.java
@@ -1,4 +1,5 @@
 package com.example.demo.profiles;
 
 public interface SystemProfile {
+    String getProfile();
 }
